4a60ce6b49ab98c5740d8611c4d5fd2a2382e760
Akil Khatri <khatriakil104@gmail.com>
2025-02-16 10:40:53 +0530

    utills: ApiError,ApiResponse, AsyncHandler

---
+++ src/index.js
@@ -0,0 +1,70 @@
+// require('dotenv').config({path:'./env'})
+import 'dotenv/config'
+import connectDB from './db/index.js'
+import app from './app.js'
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port ${process.env.PORT}`);
+    })
+})
+.catch(error => console.error('connectDB :: ',error))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express'
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on('error',(error) => {
+            console.error('connectDB :: ',error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,() => {
+            console.log(`app listening on port ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.error('connectDB :: ',error);
+        throw error
+    }
+} )()
+*/
