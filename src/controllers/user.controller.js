13b9d14c1ff5565d49f5b97213129e5b36d7e184
Akil Khatri <khatriakil104@gmail.com>
2025-03-12 10:43:57 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,7 +32,7 @@ const registerUser = asyncHandler( async (req,res) => {
     // return res
 
     const {fullName,email,username,password} = req.body
-    console.log("Request Body: ",req.body);
+    // console.log("Request Body: ",req.body);
     
     if (
         [fullName,email,username,password].some( field => field?.trim() === "" )
@@ -47,10 +47,11 @@ const registerUser = asyncHandler( async (req,res) => {
     if(existingUser){
         throw new ApiError(409,"User with email or username already exists")
     }
+    // console.log('Request Object: ',req);
     
-    // console.log("Request Files: ",req.files);
+    console.log("Request Files: ",req.files);
     
-    const avatarLocalPath = req.files?.avatar[0]?.path
+    const avatarLocalPath = req.files?.avatar?.[0]?.path
     // const coverImageLocalPath = req.files?.coverImage[0]?.path
 
     let coverImageLocalPath
